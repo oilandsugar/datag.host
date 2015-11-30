6b92b696288ac52ecb8a905b7bf3750cdb236d88
oilandsugar <tommy_pm@hotmail.com>
2015-11-29 23:55:26 -0500

    update style

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 jQuery(function($) {
-
-  function open(id){
-    console.log(id);
-  }
-
-  function open_old(id){
-    var element = $('#' + id);
-    var type = element.attr('class');
-    if(type == "img") {
-      var link = element.find("img").attr('src');
-    }
-    $('#gallery').find('.content').html('<img src="' + link + '">');
-    $('#gallery').delay(500).fadeIn(800);
-  }
-
 	$(document).ready(function() {
 
     $('#gallery').click(function(){
-      $('#gallery').fadeOut(800).find('.content').html('');
+      $('#gallery').fadeOut(800)
+      $('#gallery').delay(1000).find('.content').html('');
     });
 
     $('.img').click(function(){

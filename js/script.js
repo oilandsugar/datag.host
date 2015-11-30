661e0fac7fea0f7ad204e9e4e2d59f73b31888a3
oilandsugar <tommy_pm@hotmail.com>
2015-11-30 00:02:00 -0500

    add caption support for gallery

--- js/script.js
+++ js/script.js
@@ -10,8 +10,16 @@ jQuery(function($) {
       var type = $(this).attr('class');
       if(type == "img") {
         var link = $(this).find("img").attr('src');
+        if($(this).find('.caption').length) {
+          var caption = $(this).find('.caption').html();
+        }
       }
       $('#gallery').find('.content').html('<img src="' + link + '">');
+      if(caption) {
+        $('#gallery').find('.caption').html(caption).show();
+      } else {
+        $('#gallery').find('.caption').hide();
+      }
       $('#gallery').delay(500).fadeIn(800);
     });
 

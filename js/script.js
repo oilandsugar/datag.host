48a3e0b244936bf4f399261148c04a9a9a9f066c
oilandsugar <tommy_pm@hotmail.com>
2015-12-05 18:26:55 -0500

    fix animations on content type change and gallery add commands

--- js/script.js
+++ js/script.js
@@ -7,11 +7,31 @@ jQuery(function($) {
 	  }
 	}
 
+	function toggleSections(contentType){
+		if(contentType == "all") {
+			$('.content-type:visible').fadeOut(800, 'easeInOutCubic', function(){
+				$('.content-type').find('h1').find('span').remove();
+				$('.content-type:nth-child(1)').removeClass('full').fadeIn(800, 'easeInOutCubic');
+				$('.content-type:nth-child(2)').delay(200).removeClass('full').fadeIn(800, 'easeInOutCubic');
+				$('.content-type:nth-child(3)').delay(400).removeClass('full').fadeIn(800, 'easeInOutCubic');
+			});
+		} else {
+			$('.content-type:visible').fadeOut(800, 'easeInOutCubic', function(){
+				$('.content-type').find('h1').find('span').remove();
+				$('.content-type').removeClass('full');
+				$('#' + contentType).find('h1').prepend('<span>ONLY </span>');
+				$('#' + contentType).toggleClass('full').fadeIn(800, 'easeInOutCubic');
+			});
+		}
+	}
+
 	$(document).ready(function() {
 
     $('#gallery').click(function(){
-      $('#gallery').fadeOut(800)
-      $('#gallery').delay(1000).find('.content').html('');
+      $('#gallery').fadeOut(800, 'easeInOutCubic', function(){
+				$(this).find('.content').html('');
+			});
+
     });
 
     $('.img').click(function(){
@@ -25,7 +45,7 @@ jQuery(function($) {
       } else {
         $('#gallery').find('.caption').hide();
       }
-      $('#gallery').delay(500).fadeIn(800);
+      $('#gallery').delay(500).fadeIn(800, 'easeInOutCubic');
     });
 
 		$('.sound').click(function(e){
@@ -33,7 +53,7 @@ jQuery(function($) {
 			var link = $(this).find("a").attr('href');
 			$('#gallery').find('.content').html('<iframe width="100%" height="450" scrolling="no" frameborder="no" src="' + link + '"></iframe>');
 			$('#gallery').find('.caption').hide();
-			$('#gallery').delay(500).fadeIn(800);
+			$('#gallery').delay(500).fadeIn(800, 'easeInOutCubic');
 		});
 
 		$('#open-terminal').click(function(e){
@@ -52,7 +72,10 @@ jQuery(function($) {
 		    if(e.which == 13) {
 					var lines = $('#terminal').find('textarea').val().split('\n');
 					var input = lines[lines.length-1];
-					console.log(input);
+					var commands =["darker", "lighter", "neutral", "about", "help", "more help", "void", "visuals", "sounds", "echoes", "all content", "see more", "see less"];
+					// clear output first
+					$('#output').html('');
+					// process input
 					if(input == "darker")
 					{
 						$('#content').css('background', '#000000');
@@ -67,18 +90,15 @@ jQuery(function($) {
 					}
 					else if(input == "about")
 					{
-						$('#output').html('');
 						showText("#output", "DATAG.HOST is a visual and musical project by Tommy Poirier-Morissette, bringing code, generative visuals and electronic music into public space and back on the web.", 0, 10);
 					}
 					else if(input == "help")
 					{
-						$('#output').html('');
 						showText("#output", "You are on your own here, no documentation is yet accessible, feel free to explore this uncharted territory.", 0, 10);
 					}
 					else if(input == "more help")
 					{
-						$('#output').html('');
-						showText("#output", "Okay, I lied, here is commands to get you started: 'about' 'help' 'darker' 'lighter' 'neutral' 'void'", 0, 10);
+						showText("#output", "Okay, I lied, here are commands to get you started: 'about' 'help' 'darker' 'lighter' 'neutral' 'void' 'see more' 'see less' 'visuals' 'sounds' 'echoes' 'all content'", 0, 10);
 					}
 					else if(input == "void")
 					{
@@ -86,53 +106,36 @@ jQuery(function($) {
 					}
 					else if(input == "visuals")
 					{
-						$('#sounds').fadeOut('500').removeClass('full');
-						$('#visuals').fadeIn('500').toggleClass('full');
-						$('#echoes').fadeOut('500').removeClass('full');
-						$('#visuals').find('h1').html('only visuals');
-						$('#sounds').find('h1').html('sound');
-						$('#echoes').find('h1').html('echoes');
+						toggleSections("visuals");
 					}
 					else if(input == "sounds")
 					{
-						$('#sounds').fadeIn('500').toggleClass('full');
-						$('#visuals').fadeOut('500').removeClass('full');
-						$('#echoes').fadeOut('500').removeClass('full');
-						$('#visuals').find('h1').html('visuals');
-						$('#sounds').find('h1').html('only sound');
-						$('#echoes').find('h1').html('echoes');
+						toggleSections("sounds");
 					}
 					else if(input == "echoes")
 					{
-						$('#sounds').fadeOut('500').removeClass('full');
-						$('#visuals').fadeOut('500').removeClass('full');
-						$('#echoes').fadeIn('500').toggleClass('full');
-						$('#visuals').find('h1').html('visuals');
-						$('#sounds').find('h1').html('sound');
-						$('#echoes').find('h1').html('only echoes');
+						toggleSections("echoes");
 					}
 					else if(input == "all content")
 					{
-						$('#sounds').fadeIn('500').removeClass('full');
-						$('#visuals').fadeIn('500').removeClass('full');
-						$('#echoes').fadeIn('500').removeClass('full');
-						$('#visuals').find('h1').html('visuals');
-						$('#sounds').find('h1').html('sound');
-						$('#echoes').find('h1').html('echoes');
+						toggleSections("all");
 					}
 					else if(input == "see more")
 					{
-						$('#head').toggleClass('more-terminal');
-						$('#content').toggleClass('more-terminal');
+						$('#head').addClass('more-terminal');
+						$('#content').addClass('more-terminal');
+					}
+					else if(input == "see less")
+					{
+						$('#head').removeClass('more-terminal');
+						$('#content').removeClass('more-terminal');
 					}
 					else if(input == "batman")
 					{
-						$('#output').html('');
 						showText("#output", "NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN", 0, 10);
 					}
 					else
 					{
-						$('#output').html('');
 						showText("#output", "That's not a registered expression, try 'help' to get some insight.", 0, 10);
 					}
 		    }

33e4a44f0ff2650d4f20abe0494388f3e41d0bed
oilandsugar <tommy_pm@hotmail.com>
2016-01-01 13:25:57 -0500

    fix body class

--- dataghost-app/controllers/bodyCtrl.js
+++ dataghost-app/controllers/bodyCtrl.js
@@ -1,5 +1,8 @@
-angular.module('dataghost-app').controller("bodyCtrl", function(){
+angular.module('dataghost-app').controller("bodyCtrl", ['$rootScope', function(){
     var vm = this;
     console.log('in controller body');
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        vm.bodyClass = toState;
+    });
     vm.bodyClass = 'index';
-});
+}]);

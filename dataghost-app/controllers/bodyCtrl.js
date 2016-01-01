47b449bfd2d860bb25c80f428bdc3fe3531ef0c9
oilandsugar <tommy_pm@hotmail.com>
2016-01-01 13:53:02 -0500

    toggle logo

--- dataghost-app/controllers/bodyCtrl.js
+++ dataghost-app/controllers/bodyCtrl.js
@@ -4,10 +4,19 @@ function bodyCtrlFunc($rootScope){
     var vm = this;
     console.log('in controller body');
     $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        $rootScope.bodyClass = toState;
+        $rootScope.bodyClass = toState.name;
     });
 
     vm.getClass = function() {
-        return $rootScope.bodyClass;
+      return $rootScope.bodyClass;
     };
+
+    vm.lightOrDark = function() {
+      if(rootScope.bodyClass == 'visuals') {
+        return 'light';
+      } else {
+        return 'dark';
+      }
+    };
+
 };
